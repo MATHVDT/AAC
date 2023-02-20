0d9031e7e4320acb7164167ae0af466b53a29cfd
mavilledie4 <mavilledie4@a212pc01L.local.isima.fr>
2023-02-20 17:08:13 +0100

    deplacement

---
+++ algo2/knuthMorrisPratt.hpp
@@ -0,0 +1,11 @@
+#ifndef __KNUTH_MORRIS_PRATT__
+#define __KNUTH_MORRIS_PRATT__
+
+#include <vector>
+
+#include <iostream>
+
+int knuth_morris_pratt(const std::string &chaine,
+                       const std::string &motif);
+
+#endif
---
+++ algo2/notes.md
@@ -0,0 +1,4 @@
+# AAC algo 2 : Recherhce de motifs - Knuth-Morris-Pratt
+
+Reprise de l'algo python du livre *Programmation efficace Les 128 algorithmes qu'il faut avoir compris et codés en Python au cours de sa vie*
+
